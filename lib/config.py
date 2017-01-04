47c921a37cd851f1fd0d960003a0c9fbb4b5c657
Noah Tilton <noah@tilton.co>
2017-01-04 07:56:55 -0600

    fixups

--- lib/config.py
+++ lib/config.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from os import getcwd
-from os.path import join
-from os.path import dirname
+from os import getcwd, path
 import shlex
 
+import configparser
 # IT PROBABLY WON'T BE NECESSARY TO CHANGE THE FOLLOWING
 
-STAG_PATH       = dirname(dirname(__file__))
+STAG_PATH       = path.abspath(path.dirname(__file__))
 BASE_PATH       = getcwd()
-OUTPUT_PATH     = join(BASE_PATH, "_output")
-TEMPLATE_PATH   = join(BASE_PATH, "_templates")
+OUTPUT_PATH     = path.join(BASE_PATH, "_output")
+TEMPLATE_PATH   = path.join(BASE_PATH, "_templates")
 
-import configparser
 userconfig  = configparser.RawConfigParser()
-result      = userconfig.read(join(BASE_PATH, "stag.cfg"))
+result      = userconfig.read(path.join(BASE_PATH, "stag.cfg"))
 
 DEPLOY_PATH = TAGLINE = TITLE = URL = AUTHOR = EMAIL = DISQUS_SHORTNAME = ''
 N_POSTS = 1
@@ -27,13 +25,13 @@ else:
     URL                 = userconfig["stag"]["url"]
     AUTHOR              = userconfig["stag"]["author"]
     EMAIL               = userconfig["stag"]["email"]
-    DEPLOY_PATH         = join( BASE_PATH, userconfig["stag"]["deploy_path"] )
+    DEPLOY_PATH         = path.join( BASE_PATH, userconfig["stag"]["deploy_path"] )
     DISQUS_SHORTNAME    = userconfig["stag"]["disqus_shortname"]
     N_POSTS             = int( userconfig["stag"]["n_posts"] )
 
 
 try:
-    GOOGLE_ANALYTICS = open(join(TEMPLATE_PATH, "ga.js"), 'r').read()
+    GOOGLE_ANALYTICS = open(path.join(TEMPLATE_PATH, "ga.js"), 'r').read()
 except:
     GOOGLE_ANALYTICS = ''
 
@@ -47,12 +45,12 @@ config = {
     "deploy_path"   :   DEPLOY_PATH,
     "ga"            :   GOOGLE_ANALYTICS,
     #
-    "posts_path"    :   join(BASE_PATH, "_posts"),
+    "posts_path"    :   path.join(BASE_PATH, "_posts"),
     "template_path" :   TEMPLATE_PATH,
-    "assets_path"   :   join(BASE_PATH, "_assets"),
+    "assets_path"   :   path.join(BASE_PATH, "_assets"),
     #
-    "index_path"    :   join(OUTPUT_PATH, "index.html"),
-    "archive_path"  :   join(OUTPUT_PATH, "archive.html"),
+    "index_path"    :   path.join(OUTPUT_PATH, "index.html"),
+    "archive_path"  :   path.join(OUTPUT_PATH, "archive.html"),
     "disqus_shortname" : DISQUS_SHORTNAME,
     #
     "feed"          : {
@@ -62,7 +60,7 @@ config = {
         "description"   :   TAGLINE,
         "author_name"   :   AUTHOR,
         "author_email"  :   EMAIL,
-        "feed_url"      :   join(OUTPUT_PATH, TITLE).lower() + ".atom",
+        "feed_url"      :   path.join(OUTPUT_PATH, TITLE).lower() + ".atom",
         "language"      : "en",
     },
 }
